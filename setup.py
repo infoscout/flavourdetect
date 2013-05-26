1c74398efae30209aa450beeb804f6a8c5b09dca
Jon Brelig <jonbrelig@gmail.com>
2013-05-26 08:54:14 -0700

    Add version to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from isc_ops.setup_tools import setup, current_version
+
 
 setup(name='flavourdetect',
     packages=find_packages(),  
     description = 'Django app detects the user device and serves up the appropriate template.',
     url = 'http://github.com/infoscout/flavourdetect',
-    version = '0.1dev',    
+    version = current_version(),     
     install_requires=[
-        'django>=1.4',
+        'django==1.4',
     ]
 )
 
