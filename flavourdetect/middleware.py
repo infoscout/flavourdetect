c32a3bad2ed09d740ade2b4ba6a77da63c1c528f
Lucas Connors <lucas.revolutiontech@gmail.com>
2017-05-10 16:40:58 -0700

    CA-6022 Add support for Django 1.10 MIDDLEWARE setting

--- flavourdetect/middleware.py
+++ flavourdetect/middleware.py
@@ -1,7 +1,12 @@
+try:
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:  # Django < 1.10 support
+    MiddlewareMixin = object
+
 from flavourdetect.utils import flavour_detect
 
 
-class FlavourDetectMiddleware(object):
+class FlavourDetectMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
         request.flavour = flavour_detect(request)
